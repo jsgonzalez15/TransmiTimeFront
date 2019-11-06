edee3bf6ca79502f6625f77df69e5b662860e2d7
Sergio Cárdenas <se.cardenas@uniandes.edu.co>
2019-11-05 19:52:54 -0500

    correcciones

--- App.js
+++ App.js
@@ -10,29 +10,7 @@ import ScreenSearchView from './components/ScreenSearchView';
 /*Navegation imports*/
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-/*Firebase imports*/
-import firebase from 'firebase';
 
-componentWillMount() {
-  var firebaseConfig = {
-    apiKey: "AIzaSyA9HOqxXdnE3bA9GYYk4aN4Wt_dkjGSkco",
-    authDomain: "basedatosprueba1.firebaseapp.com",
-    databaseURL: "https://basedatosprueba1.firebaseio.com",
-    projectId: "basedatosprueba1",
-    storageBucket: "basedatosprueba1.appspot.com",
-    messagingSenderId: "174995287278",
-    appId: "1:174995287278:web:16e61d006ef90aac5757ff",
-    measurementId: "G-F1WPHNHSHY"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  // Mensajes de cultura y consejos en vagón obtenidos solo al iniciar la aplicación
-  firebase.database().ref('Cultura').once('value',(CulturaObject)=>{});
-  // Objeto de usuarios por tiempo obtenidos siempre que sean actualizados
-  firebase.database().ref('DatosPublicosTransmilenio/Ruta1/Puerta3/DatosRegresion').on('value',(PeopleObject)=>{});
-  // Número de personas actual obtenido siempre que sea actualizado
-  firebase.database().ref('DatosPublicosTransmilenio/Ruta1/Puerta3/Personas').on('value',(PeopleNowObject)=>{});
-}
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
@@ -61,64 +39,12 @@ class App extends Component {
 
 export default App;*/
 
-
-
-class MainScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ScreenMainView/>
-      </View>
-    );
-  }
-}
-
-class StationScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ScreenStationView/>
-      </View>
-    );
-  }
-}
-
-class TimeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ScreenTimeView/>
-      </View>
-    );
-  }
-}
-
-class SearchScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ScreenSearchView/>
-      </View>
-    );
-  }
-}
-
-class CultureScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ScreenCultureView/>
-      </View>
-    );
-  }
-}
-
 const RootStack = createStackNavigator({
-  MAIN: MainScreen,
-  STATION: StationScreen,
-  SEARCH: SearchScreen,
-  TIME: TimeScreen,
-  CULTURE: CultureScreen
+  MAIN: ScreenMainView,
+  STATION: ScreenStationView,
+  SEARCH: ScreenSearchView,
+  TIME: ScreenTimeView,
+  CULTURE: ScreenCultureView
 });
 
 export default createAppContainer(RootStack);
--- components/ScreenMainView.js
+++ components/ScreenMainView.js
@@ -1,68 +1,94 @@
 import React, {useState, Component } from 'react';
 import {View, StyleSheet, Text, TouchableOpacity, Button} from 'react-native';
 import Header from './Header';
+/*Firebase imports*/
+import firebase from 'firebase';
 
 
-const ScreenMainView = props => {
+class ScreenMainView extends Component {
+
+	componentWillMount() {
+		var firebaseConfig = {
+			apiKey: "AIzaSyA9HOqxXdnE3bA9GYYk4aN4Wt_dkjGSkco",
+			authDomain: "basedatosprueba1.firebaseapp.com",
+			databaseURL: "https://basedatosprueba1.firebaseio.com",
+			projectId: "basedatosprueba1",
+			storageBucket: "basedatosprueba1.appspot.com",
+			messagingSenderId: "174995287278",
+			appId: "1:174995287278:web:16e61d006ef90aac5757ff",
+			measurementId: "G-F1WPHNHSHY"
+		};
+		// Initialize Firebase
+		firebase.initializeApp(firebaseConfig);
+		// Mensajes de cultura y consejos en vagón obtenidos solo al iniciar la aplicación
+		firebase.database().ref('Cultura').once('value',(CulturaObject)=>{});
+		// Objeto de usuarios por tiempo obtenidos siempre que sean actualizados
+		firebase.database().ref('DatosPublicosTransmilenio/Ruta1/Puerta3/DatosRegresion').on('value',(PeopleObject)=>{});
+		// Número de personas actual obtenido siempre que sea actualizado
+		firebase.database().ref('DatosPublicosTransmilenio/Ruta1/Puerta3/Personas').on('value',(PeopleNowObject)=>{});
+	}
+
+  render() {
     return(
     <View>
-        <View>
-            <Header title="1 Portal El dorado" subtitle="Universidades-vagón 2"/>
-         </View>
-        <TouchableOpacity /*onPress={() => this.props.navigation.navigate('STATION')}*/>
-        <View style={styles.usersNow}>
-            <Text style={styles.subsubtitle}>Usuarios ahora:</Text>
-        </View>
-        </TouchableOpacity>
-        <TouchableOpacity /*onPress={() => this.props.navigation.navigate('TIME')}*/>
-        <View style={styles.usersToday}>
-            <Text style={styles.subsubtitle}>Usuarios hoy:</Text>
-        </View>
-        </TouchableOpacity>
-        <View style={styles.busesLeft}>
-            <Text style={styles.subsubtitle}>Buses a esperar:</Text>
-        </View>
-        <View style={styles.companions}>
-            <Text style={styles.subsubtitle}>¿Cuántas personas más van contigo?</Text>
-            <Button title='+' /*onPress={()=> aumentarAcompañantes}*//>
-            <Button title='-' /*onPress={()=> disminuirAcompañantes}*//>
-        </View>
-        <View style={styles.searchService}>
-            <Button title='Buscar servicio en Mapa' /*onPress={() => this.props.navigation.navigate('SEARCH')}*//>
-        </View>
+      <View>
+        <Header title="1 Portal El dorado" subtitle="Universidades-vagón 2"/>
+      </View>
+      <TouchableOpacity onPress={() => this.props.navigation.navigate('STATION')}>
+      <View style={styles.usersNow}>
+        <Text style={styles.subsubtitle}>Usuarios ahora:</Text>
+      </View>
+      </TouchableOpacity>
+      <TouchableOpacity /*onPress={() => this.props.navigation.navigate('TIME')}*/>
+      <View style={styles.usersToday}>
+        <Text style={styles.subsubtitle}>Usuarios hoy:</Text>
+      </View>
+      </TouchableOpacity>
+      <View style={styles.busesLeft}>
+        <Text style={styles.subsubtitle}>Buses a esperar:</Text>
+      </View>
+      <View style={styles.companions}>
+        <Text style={styles.subsubtitle}>¿Cuántas personas más van contigo?</Text>
+        <Button title='+' /*onPress={()=> aumentarAcompañantes}*//>
+        <Button title='-' /*onPress={()=> disminuirAcompañantes}*//>
+      </View>
+      <View style={styles.searchService}>
+        <Button title='Buscar servicio en Mapa' /*onPress={() => this.props.navigation.navigate('SEARCH')}*//>
+      </View>
     </View>
     );
-};
+  }
+}
 
 const styles = StyleSheet.create({
-    usersNow:{
-        height:60,
-        fontSize:18,
-        paddingTop:10
-    },
-    usersToday:{
-        height:80,
-        fontSize:18,
-        paddingVertical:40
-    },
-    busesLeft:{
-        height:80,
-        fontSize:18,
-        paddingEnd:10
-    },
-    companions:{
-        fontSize:18,
-        height:80
-    },
-    searchService:{
-        fontSize:18,
-        height:40,
-        paddingVertical:5
-    },
-    subsubtitle:{
-        fontSize:18,
-        fontFamily: 'serif'
-    }
+	usersNow:{
+		height:60,
+		fontSize:18,
+		paddingTop:10
+	},
+	usersToday:{
+		height:80,
+		fontSize:18,
+		paddingVertical:40
+	},
+	busesLeft:{
+		height:80,
+		fontSize:18,
+		paddingEnd:10
+	},
+	companions:{
+		fontSize:18,
+		height:80
+	},
+	searchService:{
+		fontSize:18,
+		height:40,
+		paddingVertical:5
+	},
+	subsubtitle:{
+		fontSize:18,
+		fontFamily: 'serif'
+	}
 });
 
 export default ScreenMainView;
